1ed2f5041a228af4f8deb1ed301f86627b471b3a
led-x <day_jason@hotmail.com>
2016-06-18 21:39:21 -0400

    Add files via upload

---
+++ iontest/www/js/app.js
@@ -0,0 +1,108 @@
+var ChampApp = angular.module('ChampApp', ['ionic','ui.router'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+ChampApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('outside', {
+    url: '/outside',
+    abstract: true,
+    templateUrl: 'templates/outside.html'
+  })
+  .state('outside.login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('outside.register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+    controller: 'RegisterCtrl'
+  })
+  .state('outside.findteam', {
+  url: '/findteam',
+  templateUrl: 'templates/findteam.html',
+  controller: 'TeamCtrl'
+  })
+  .state('outside.create', {
+  url: '/create',
+  templateUrl: 'templates/create.html',
+  controller: 'TeamCtrl'
+  })
+    .state('list',{
+    url:'/list',
+    controller: 'ListController',
+    templateUrl: 'templates/list.html'
+  })
+   .state('hub',{
+    //:playerId or use id will need to be passed here
+    url:'/hub',
+    controller: 'HubCtrl',
+    templateUrl: 'templates/inside.html'
+  })
+  .state('players',{
+    //:playerId or use id will need to be passed here
+    url:'/players',
+    controller: 'PlayerController',
+    templateUrl: 'templates/players.html'
+  })
+  .state('matchhistory',{
+    //:playerId or use id will need to be passed here
+    url:'/matchhistory',
+    controller: 'MHController',
+    templateUrl: 'templates/matchhistory.html'
+  })
+  .state('teamcomp',{
+    //:playerId or use id will need to be passed here
+    url:'/teamcomp',
+    controller: 'TCController',
+    templateUrl: 'templates/teamcomp.html'
+  })
+  .state('map',{
+    //:playerId or use id will need to be passed here
+    url:'/map',
+    controller: 'MapController',
+    templateUrl: 'templates/map.html'
+  })
+.state('details',{
+    url:'/details/:id',
+    controller: 'DetailsController',
+    templateUrl: 'templates/details.html'
+  })
+ .state('skins',{
+    url:'/skins/:id',
+    controller: 'SkinsController',
+    templateUrl: 'templates/skins.html'
+  });
+
+  $urlRouterProvider.otherwise('/outside/login');
+})
+
+ChampApp.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+    if (!AuthService.isAuthenticated()) {
+      console.log(next.name);
+      if (next.name !== 'outside.login' && next.name !== 'outside.register') {
+        event.preventDefault();
+        $state.go('outside.login');
+      }
+    }
+  });
+});
---
+++ iontest/www/js/services.js
@@ -0,0 +1,140 @@
+angular.module('ChampApp')
+
+.service('AuthService', function($q, $http, API_ENDPOINT) {
+  var LOCAL_TOKEN_KEY = 'yourTokenKey';
+  var isAuthenticated = false;
+  var authToken;
+
+  function loadUserCredentials() {
+    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+    if (token) {
+      useCredentials(token);
+    }
+  }
+
+  function storeUserCredentials(token) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+    useCredentials(token);
+  }
+
+  function useCredentials(token) {
+    isAuthenticated = true;
+    authToken = token;
+
+    // Set the token as header for your requests!
+    $http.defaults.headers.common.Authorization = authToken;
+  }
+
+  function destroyUserCredentials() {
+    authToken = undefined;
+    isAuthenticated = false;
+    $http.defaults.headers.common.Authorization = undefined;
+    window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+  }
+ 
+  var addTeam = function(user){
+    return $q(function(resolve, reject) {
+      $http.post(API_ENDPOINT.url + '/addTeam', user).then(function(result) {
+        if (result.data.success) {
+          storeUserCredentials(result.data.token);
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };
+
+  var findTeams = function(team){
+    return $q(function(resolve, reject) {
+      $http.get(API_ENDPOINT.url + '/findteams', team).then(function(result) {
+        if (result.data.success) {
+          storeUserCredentials(result.data.token);
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  }
+
+    var getKey = function(key){
+    return $q(function(resolve, reject) {
+      $http.post(API_ENDPOINT.url + '/getkey', key).then(function(result) {
+        if (result.data.success) {
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  }
+
+
+  var addUserTeam = function(user){
+  return $q(function(resolve, reject) {
+    $http.post(API_ENDPOINT.url + '/addTeam', user).then(function(result) {
+      if (result.data.success) {
+        storeUserCredentials(result.data.token);
+        resolve(result.data.msg);
+      } else {
+        reject(result.data.msg);
+      }
+    });
+  });
+};
+
+  var register = function(user) {
+    return $q(function(resolve, reject) {
+      $http.post(API_ENDPOINT.url + '/signup', user).then(function(result) {
+        if (result.data.success) {
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };
+
+  var login = function(user) {
+    return $q(function(resolve, reject) {
+      $http.post(API_ENDPOINT.url + '/authenticate', user).then(function(result) {
+        if (result.data.success) {
+          storeUserCredentials(result.data.token);
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };
+
+  var logout = function() {
+    destroyUserCredentials();
+  };
+
+  loadUserCredentials();
+
+  return {
+    login: login,
+    register: register,
+    addTeam: addTeam,
+    logout: logout,
+    isAuthenticated: function() {return isAuthenticated;},
+  };
+})
+
+.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+  return {
+    responseError: function (response) {
+      $rootScope.$broadcast({
+        401: AUTH_EVENTS.notAuthenticated,
+      }[response.status], response);
+      return $q.reject(response);
+    }
+  };
+})
+
+.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor');
+});
